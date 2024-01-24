d7d4960b00f319477bad6cca19ee14996c02bda4
Denis Kuso <sourcework.online@keemail.me>
2024-01-24 16:17:55 +0000

    add readiness and error endpoints

--- main.go
+++ main.go
@@ -5,10 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/cors"
-	//"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 func main() {
@@ -18,33 +17,51 @@ func main() {
 		log.Fatal("Failed loading enviroment.")
 	}
 	port := os.Getenv(PORT)
+	ready := "/readiness"
+	errorEndpoint := "/err"
+
 	r := chi.NewRouter()
 	apiRouter := chi.NewRouter()
 
+	r.Use(middleware.Logger)
+	r.Use(middleware.URLFormat)
 	// Use default options for now
 	r.Use(cors.Default().Handler)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		w.Write([]byte("welcome Gandalf"))
 	})
 	r.Mount("/v1",apiRouter)
+	apiRouter.Get(ready, func(w http.ResponseWriter, r *http.Request) {
+		respondWithJSON(w, http.StatusOK, "status:ok" )
+		})	
+	apiRouter.Get(errorEndpoint, func(w http.ResponseWriter, r *http.Request){
+		respondWithError(w, http.StatusInternalServerError, "Internal server error")
+		})
 
-	http.ListenAndServe(":"+port, r)
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: r,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	server.ListenAndServe()
+	//http.ListenAndServe(":"+port, r)
 }
 
-func MiddlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		log.Printf("Method: %v; URL:%v", r.Method, r.URL)
-		next.ServeHTTP(w, r)
-	})
-}
+//func MiddlewareCors(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		w.Header().Set("Access-Control-Allow-Origin", "*")
+//		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+//		w.Header().Set("Access-Control-Allow-Headers", "*")
+//		if r.Method == "OPTIONS" {
+//			w.WriteHeader(http.StatusOK)
+//			return
+//		}
+//		log.Printf("Method: %v; URL:%v", r.Method, r.URL)
+//		next.ServeHTTP(w, r)
+//	})
+//}
 
 func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
