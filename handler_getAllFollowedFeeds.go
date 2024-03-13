50870c4a2b15142c0755cf1da4ffe17bba8457d2
Denis Kuso <sourcework.online@keemail.me>
2024-03-13 16:22:50 +0000

    add error granularity when retriving all followed feed

--- handler_getAllFollowedFeeds.go
+++ handler_getAllFollowedFeeds.go
@@ -1,21 +1,30 @@
-package main 
+package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/Denis-Kuso/rss_aggregator_p/internal/database"
 )
 
 func (s *stateConfig) GetAllFollowedFeeds(w http.ResponseWriter, r *http.Request, user database.User) {
 
+	var errMsg string
 	feedFollows, err := s.DB.GetFeedFollowsForUser(r.Context(), user.ID)
 	if err != nil {
-		log.Printf("ERR during retriveal of all feedFollows: %v\n", err)
-		respondWithError(w, http.StatusInternalServerError,"can't retrieve feedfollows")
+		if errors.Is(err, sql.ErrNoRows) {
+			errMsg = fmt.Sprintf("no followed feeds found")
+			respondWithJSON(w, http.StatusOK, errMsg)
+			return
+		}
+		errMsg = "err retrieving all feedFollows"
+		log.Printf("%s: %v\n", errMsg, err)
+		respondWithError(w, http.StatusInternalServerError, "can't retrieve feedfollows")
 		return
 	}
-	// IF NOT entries in feed-follows for this user?
 	respondWithJSON(w, http.StatusOK, feedFollows)
 	return
 }
-
