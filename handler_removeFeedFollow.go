777ad4b432a4f4c7263ed702af2433195811ac30
Denis Kuso <sourcework.online@keemail.me>
2024-03-13 17:42:30 +0000

    add error granularity when unfollowing a feed

--- handler_removeFeedFollow.go
+++ handler_removeFeedFollow.go
@@ -1,40 +1,53 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/Denis-Kuso/rss_aggregator_p/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
 
-func (s *stateConfig) UnfollowFeed(w http.ResponseWriter, r *http.Request, user database.User){
+func (s *stateConfig) UnfollowFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	var errMsg string
 	providedFeedFollowID := chi.URLParam(r, QUERY_FEED_FOLLOW)
 
 	feedFollowID, err := uuid.Parse(providedFeedFollowID)
-	if err != nil{
-		log.Printf("ERR during Feed-Follow id converstion to UUID: %v\n", err)
-		respondWithError(w, http.StatusInternalServerError,"Cannot proces feed id")
+	if err != nil {
+		errMsg = fmt.Sprintf("Cannot parse feed id: %s", providedFeedFollowID)
+		log.Printf("%s; err: %v\n", errMsg, err)
+		respondWithError(w, http.StatusInternalServerError, errMsg)
 		return
 	}
 	// Does feed_follow even exist?
 	_, err = s.DB.GetFeedFollow(r.Context(), feedFollowID)
 	if err != nil {
-		log.Printf("%v. FeedFollow does not exists, cannot delete feedFollow :%v.\n",err, providedFeedFollowID)
-		respondWithError(w, http.StatusBadRequest,"No feed follow for this user and feed\n")
+		if errors.Is(err, sql.ErrNoRows) {
+			errMsg = fmt.Sprintf("id: %s does not exist", providedFeedFollowID)
+			respondWithError(w, http.StatusNotFound, errMsg)
+			return
+		}
+		errMsg = fmt.Sprintf("cannot delete feedFollow: %s", providedFeedFollowID)
+		log.Printf("%s; err: %v.", errMsg, err)
+		respondWithError(w, http.StatusInternalServerError, errMsg)
 		return
 	}
-	
+
 	// err would be nil on non-existing entry
 	err = s.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
-		ID: feedFollowID,
+		ID:     feedFollowID,
 		UserID: user.ID,
 	})
 	if err != nil {
-		log.Printf("ERR during removal of Feed-followd from db: %v\n", err)
-		respondWithError(w, http.StatusInternalServerError, "cannot remove link to feed")
+		errMsg = fmt.Sprintf("cannot unfollow feed: %s", providedFeedFollowID)
+		log.Printf("%s; err: %v\n", errMsg, err)
+		respondWithError(w, http.StatusInternalServerError, errMsg)
 		return
 	}
-	respondWithJSON(w, http.StatusOK,"Unfollowed feed")
+	respondWithJSON(w, http.StatusOK, "Unfollowed feed")
 	return
 }
