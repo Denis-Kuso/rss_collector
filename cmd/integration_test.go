0e4cf13a0b82cc3582053392f2f663e46cf44a57
Denis Kuso <sourcework.online@keemail.me>
2024-03-15 17:00:58 +0000

    add draft integration test

--- cmd/integration_test.go
+++ cmd/integration_test.go
@@ -1,9 +1,10 @@
-//go:build integration
-
 package cmd
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"testing"
 )
 
 // flow of tests:
@@ -19,6 +20,108 @@ import (
 // get user data
 // get posts
 
+// Do it without creating a user first, need to implement
+// saving apikey, then retrieving...
 func TestIntegration(t *testing.T) {
 	fmt.Println("TESTING with real server...")
+	fmt.Println("Ou jea, let's expose some flaws")
+	//fmt.Println("Creating user...")
+	//fmt.Println("User created!")
+	type User struct {
+		name string
+		key  string
+	}
+	user := User{
+		name: "Pobro",
+		key:  "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c",
+	}
+	t.Run("Get existing user data", func(t *testing.T) {
+		expOut := `{"ID":"a42113b1-ed14-458a-aecb-c42c0c17fbda","CreatedAt":"2024-02-02T17:09:44.334957Z","UpdatedAt":":"2024-02-02T17:09:44.334957Z","Name":"Pobro","ApiKey":"6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"}`
+		var out bytes.Buffer
+		err := getUserDataAction(&out, ROOT_URL, user.key)
+		if err != nil {
+			t.Fatalf("Expected no error, got: %v\n", err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	t.Run("Get user feeds", func(t *testing.T) {
+		expOut := `TODO`
+		var out bytes.Buffer
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := getUserDataAction(&out, ROOT_URL, key)
+		if err != nil {
+			if errors.Is(err, ErrInvalidResponse) {
+				t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+			}
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	t.Run("Get feed_follows", func(t *testing.T){
+		expOut := `TODO`
+		var out bytes.Buffer
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := getAllFollowedFeedsAction(&out, ROOT_URL,key)
+		if err != nil {
+			t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	t.Run("Get posts - no limit provided", func(t *testing.T){
+		expOut := `TODO`
+		var out bytes.Buffer
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := getPostsAction(&out, ROOT_URL, key,"")
+		if err != nil {
+			t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	t.Run("Create feed", func(t *testing.T){
+		expOut := `TODO`
+		var out bytes.Buffer
+		args := []string{"feedname","feedURL"}// TODO
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := addFeedAction(&out, args, ROOT_URL, key)
+		if err != nil {
+			t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	// Get user data here?
+	t.Run("Follow feed", func(t *testing.T){
+		expOut := `TODO`
+		var out bytes.Buffer
+		feed_id := "feedID"
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := followFeedAction(&out, feed_id, ROOT_URL, key)
+		if err != nil {
+			t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
+	t.Run("Unfollow feed", func(t *testing.T){
+		expOut := `TODO`
+		var out bytes.Buffer
+		feed_id := "feedID"
+		key := "6711c5359a5bb4a60bfd37113689bc003e128764d2599a7974fbc77e1580c27c"
+		err := deleteFollowFeedAction(&out,  ROOT_URL, key, feed_id)
+		if err != nil {
+			t.Fatalf("Expected error: %v, got: %v\n", ErrInvalidResponse, err)
+		}
+		if expOut != out.String() {
+			t.Fatalf("Expected output: %v, got: %s\n", expOut, out.String())
+		}
+	})
 }
