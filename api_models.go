ce46f02c754a6bd5bd435c6e85a24554580daa63
Denis Kuso <sourcework.online@keemail.me>
2024-04-09 11:35:08 +0100

    add repr for public api

---
+++ api_models.go
@@ -0,0 +1,58 @@
+// convert database models to public api representation
+package main
+
+import (
+	"github.com/Denis-Kuso/rss_aggregator_p/internal/database"
+	"github.com/google/uuid"
+)
+
+type PublicUser struct {
+	Name   string       `json:"name"`
+	ApiKey string       `json:"apiKey"`
+	Feeds  []PublicFeed `json:"followedFeeds"`
+}
+
+func dbUserToPublicUser(user database.User, feeds []database.Feed) PublicUser {
+	f := dbFeedToPublicFeeds(feeds)
+	return PublicUser{
+		Name:   user.Name,
+		ApiKey: user.ApiKey,
+		Feeds:  f,
+	}
+}
+
+type PublicFeed struct {
+	Name string    `json:"name"`
+	URL  string    `json:"url"`
+	ID   uuid.UUID `json:"id"`
+}
+
+func dbFeedToPublicFeed(feed database.Feed) PublicFeed {
+	return PublicFeed{
+		Name: feed.Name,
+		URL:  feed.Url,
+		ID:   feed.ID,
+	}
+}
+func dbFeedToPublicFeeds(feeds []database.Feed) []PublicFeed {
+	SIZE := len(feeds)
+	f := make([]PublicFeed, SIZE)
+	for _, feed := range feeds {
+		f = append(f, dbFeedToPublicFeed(feed))
+	}
+	return f
+}
+
+type PublicPost struct {
+	FeedName string `json:"feedName"`
+	Title    string `json:"title"`
+	URL      string `json:"url"`
+}
+
+func dbPostToPublicPost(post database.Post, feed database.Feed) PublicPost {
+	return PublicPost{
+		FeedName: feed.Name,
+		Title:    post.Title,
+		URL:      post.Url,
+	}
+}
