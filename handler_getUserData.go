ff950e93fec4293916b2916ca6c55c854ad2b6ac
Denis Kuso <sourcework.online@keemail.me>
2024-04-11 11:31:42 +0100

    change response for user data retrieval

--- handler_getUserData.go
+++ handler_getUserData.go
@@ -3,12 +3,39 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"database/sql"
+	"errors"
+	"github.com/google/uuid"
 	"github.com/Denis-Kuso/rss_aggregator_p/internal/database"
 )
 
 func (s *stateConfig) GetUserData(w http.ResponseWriter, r *http.Request, user database.User){
 
-	log.Printf("Succesful retriveal of user data %v\n", user)
-	respondWithJSON(w, http.StatusOK, user)
+	var errMsg string
+	feedFollows, err := s.DB.GetFeedFollowsForUser(r.Context(), user.ID)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+		errMsg = "err retrieving feed follows"
+		respondWithError(w, http.StatusInternalServerError, "can't retrieve feedfollows")
+		return
+		}
+	}
+	SIZE := len(feedFollows)
+	feedIDs := make([]uuid.UUID, SIZE)
+	for i, f := range feedFollows{
+		feedIDs[i] =  f.FeedID
+	}
+	feeds := make([]database.Feed, SIZE)
+	feeds, err = s.DB.GetBasicInfoFeed(r.Context(), feedIDs)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			errMsg = fmt.Sprintf("cannot retrieve feed info: %v", err)
+			respondWithError(w, http.StatusInternalServerError, errMsg)
+			return
+		}
+	}
+	publicUser := dbUserToPublicUser(user,feeds)
+	respondWithJSON(w, http.StatusOK, publicUser)
 	return
 }
