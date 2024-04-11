a864a83bb5f9ce999346c3d63c62597156301b52
Denis Kuso <sourcework.online@keemail.me>
2024-04-11 12:09:06 +0100

    change response for followed feeds retrieval

--- handler_getAllFollowedFeeds.go
+++ handler_getAllFollowedFeeds.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/Denis-Kuso/rss_aggregator_p/internal/database"
+	"github.com/google/uuid"
 )
 
 func (s *stateConfig) GetAllFollowedFeeds(w http.ResponseWriter, r *http.Request, user database.User) {
@@ -20,11 +20,23 @@ func (s *stateConfig) GetAllFollowedFeeds(w http.ResponseWriter, r *http.Request
 			respondWithJSON(w, http.StatusOK, errMsg)
 			return
 		}
-		errMsg = "err retrieving all feedFollows"
-		log.Printf("%s: %v\n", errMsg, err)
-		respondWithError(w, http.StatusInternalServerError, "can't retrieve feedfollows")
+		errMsg = fmt.Sprintf("err retrieving feedFollows: %v", err)
+		respondWithError(w, http.StatusInternalServerError, errMsg)
 		return
 	}
-	respondWithJSON(w, http.StatusOK, feedFollows)
+	feedIDs := make([]uuid.UUID, len(feedFollows))
+	for i, f := range feedFollows{
+		feedIDs[i] = f.FeedID
+	}
+	feeds, err := s.DB.GetBasicInfoFeed(r.Context(), feedIDs)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			errMsg := fmt.Sprintf("cannot retrieve info about feeds: %s", err)
+			respondWithError(w, http.StatusInternalServerError, errMsg)
+			return
+		}
+	}
+	publicFeeds := dbFeedToPublicFeeds(feeds)
+	respondWithJSON(w, http.StatusOK, publicFeeds)
 	return
 }
