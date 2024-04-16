94984ad12c773033c098bce190af126db82b577d
Denis Kuso <sourcework.online@keemail.me>
2024-04-16 13:16:54 +0100

    modify handling of unfollowing a feed

--- handler_removeFeedFollow.go
+++ handler_removeFeedFollow.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,26 +18,12 @@ func (s *stateConfig) UnfollowFeed(w http.ResponseWriter, r *http.Request, user 
 	if err != nil {
 		errMsg = fmt.Sprintf("Cannot parse feed id: %s", providedFeedFollowID)
 		log.Printf("%s; err: %v\n", errMsg, err)
-		respondWithError(w, http.StatusInternalServerError, errMsg)
-		return
-	}
-	// Does feed_follow even exist?
-	_, err = s.DB.GetFeedFollow(r.Context(), feedFollowID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			errMsg = fmt.Sprintf("id: %s does not exist", providedFeedFollowID)
-			respondWithError(w, http.StatusNotFound, errMsg)
-			return
-		}
-		errMsg = fmt.Sprintf("cannot delete feedFollow: %s", providedFeedFollowID)
-		log.Printf("%s; err: %v.", errMsg, err)
-		respondWithError(w, http.StatusInternalServerError, errMsg)
+		respondWithError(w, http.StatusBadRequest, errMsg)
 		return
 	}
 
-	// err would be nil on non-existing entry
 	err = s.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
-		ID:     feedFollowID,
+		FeedID: feedFollowID,
 		UserID: user.ID,
 	})
 	if err != nil {
