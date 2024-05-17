c536a8c58ad5480a92cc0637062ec2529a039580
Denis Kuso <sourcework.online@keemail.me>
2024-05-17 18:48:04 +0100

    Refactor request checking into single function
    Every test for each action had the exact same function definition
    checking if the request for each action is well formed. Defined a
    function to reduce duplication.

--- client/cmd/actions_test.go
+++ client/cmd/actions_test.go
@@ -12,63 +12,88 @@ import (
 	"testing"
 )
 
-func TestGetUserData(t *testing.T) {
-	expURLPath := "/users"
-	expHTTPMethod := http.MethodGet
-	expAuthMethod := "ApiKey" // TODO perhaps use const or enum
-	expContentType := ""
-	expBody := ""
-	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
-	testCases := []struct {
-		name     string
-		expError error
-		expOut   string
-		resp     struct {
-			Status int
-			Body   string
+type ExpReq struct {
+	ExpContentType string
+	ExpBody        string
+	ExpUrlPath     string
+	ExpAuthMethod  string
+	ExpHTTPMethod  string
+}
+
+type Resp struct {
+	Status int
+	Body   string
+}
+type TestCase struct {
+	name     string
+	expError error
+	expOut   string
+	expReq   ExpReq
+	limit    string
+	feedID   string
+	feedName string
+	feedURL  string
+	username string
+	resp     struct {
+		Status int
+		Body   string
+	}
+}
+
+func checkReq(t *testing.T, e ExpReq, tc TestCase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != (e.ExpUrlPath) {
+			t.Errorf("Poor request! Expected path: %q, got: %q", e.ExpUrlPath, r.URL.Path)
 		}
-	}{
+		if r.Method != e.ExpHTTPMethod {
+			t.Errorf("Poor request! Expected method: %q, got: %q", e.ExpHTTPMethod, r.Method)
+		}
+		authValue := r.Header.Get("Authorization")
+		if authValue == "" {
+			t.Fatal("No header provided")
+		}
+		authMethod := strings.Split(authValue, " ")[0]
+		if authMethod != e.ExpAuthMethod {
+			t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", e.ExpAuthMethod, authMethod)
+		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		r.Body.Close()
+		if string(body) != e.ExpBody {
+			t.Errorf("Poor request! Expected body: %q, got: %q", e.ExpBody, string(body))
+		}
+		contentType := r.Header.Get("Content-Type")
+		if contentType != e.ExpContentType {
+			t.Errorf("Poor request! Expected Content-Type: %q, got: %q", e.ExpContentType, contentType)
+		}
+		w.WriteHeader(tc.resp.Status)
+		fmt.Fprintln(w, tc.resp.Body)
+	}
+}
+func TestGetUserData(t *testing.T) {
+	e := ExpReq{
+		ExpUrlPath:     "/users",
+		ExpHTTPMethod:  http.MethodGet,
+		ExpAuthMethod:  "ApiKey",
+		ExpContentType: "",
+		ExpBody:        "",
+	}
+	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
+	testCases := []TestCase{
 		{
 			name:     "get_user_data",
 			expError: nil,
+			expReq:   e,
 			expOut:   `{"ID":"someID","CreatedAt":"someTime","UpdatedAt":"someTime","Name":"testName","ApiKey":"1337"}` + string('\n'),
 			resp:     testResp[GET_USERS_DATA],
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != (expURLPath) {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, tc.expReq, tc))
 			defer cleanup()
 			var out bytes.Buffer
 			log.Printf("calling action with url: %v\n", url)
@@ -85,28 +110,20 @@ func TestGetUserData(t *testing.T) {
 			if tc.expOut != out.String() {
 				t.Errorf("Expected: %q, \n\tgot: %q\n", tc.expOut, out.String())
 			}
-
 		})
 	}
 }
 
 func TestGetPosts(t *testing.T) {
-	expURLPath := "/posts"
-	expHTTPMethod := http.MethodGet
-	expAuthMethod := "ApiKey" // TODO perhaps use const or enum
-	expContentType := ""
-	expBody := ""
+	e := ExpReq{
+		ExpUrlPath:     "/posts",
+		ExpHTTPMethod:  http.MethodGet,
+		ExpAuthMethod:  "ApiKey", // TODO perhaps use const or enum
+		ExpContentType: "",
+		ExpBody:        "",
+	}
 	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
-	testCases := []struct {
-		name     string
-		expError error
-		expOut   string
-		limit    string
-		resp     struct {
-			Status int
-			Body   string
-		}
-	}{
+	testCases := []TestCase{
 		{
 			name:     "get posts - no limit provided",
 			limit:    "",
@@ -142,38 +159,7 @@ func TestGetPosts(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != (expURLPath) {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			var out bytes.Buffer
 			err := getPostsAction(&out, url, ApiKey, tc.limit)
@@ -194,69 +180,31 @@ func TestGetPosts(t *testing.T) {
 }
 
 func TestDeleteFeedFollow(t *testing.T) {
-	expURLPath := "/feed_follows/"
-	expHTTPMethod := http.MethodDelete
-	expAuthMethod := "ApiKey" // TODO perhaps use const or enum
-	expContentType := ""
-	expBody := ""
+	e := ExpReq{
+		ExpUrlPath:     "/feed_follows/",
+		ExpHTTPMethod:  http.MethodDelete,
+		ExpAuthMethod:  "ApiKey", // TODO perhaps use const or enum
+		ExpContentType: "",
+		ExpBody:        "",
+	}
 	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
-	testCases := []struct {
-		name         string
-		expError     error
-		expOut       string
-		feedFollowID string
-		resp         struct {
-			Status int
-			Body   string
-		}
-	}{
+	testCases := []TestCase{
 		{
-			name:         "delete existing feed_follow",
-			feedFollowID: "1337",
-			expError:     nil,
-			expOut:       `{"Unfollowed feed"}` + string('\n'),
-			resp:         testResp[DELETE_FOLLOW_FEED],
+			name:     "delete existing feed_follow",
+			feedID:   "1337",
+			expError: nil,
+			expOut:   `{"Unfollowed feed"}` + string('\n'),
+			resp:     testResp[DELETE_FOLLOW_FEED],
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != (expURLPath + tc.feedFollowID) {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			fmt.Print("Testing")
 			fmt.Println(url)
 			var out bytes.Buffer
-			err := deleteFollowFeedAction(&out, url, ApiKey, tc.feedFollowID)
+			err := deleteFollowFeedAction(&out, url, ApiKey, tc.feedID)
 			if err != nil {
 				if tc.expError == nil {
 					t.Fatalf("Expected no error, got: %q.\n", err)
@@ -268,28 +216,22 @@ func TestDeleteFeedFollow(t *testing.T) {
 			if tc.expOut != out.String() {
 				t.Errorf("Expected: %q, \n\tgot: %q\n", tc.expOut, out.String())
 			}
-
 		})
 	}
 }
 
 func TestGetAllFollowedFeeds(t *testing.T) {
-	expURLPath := "/feed_follows"
-	expHTTPMethod := http.MethodGet
-	expAuthMethod := "ApiKey"     // TODO perhaps use const or enum
+	e := ExpReq{
+		ExpUrlPath:     "/feed_follows",
+		ExpHTTPMethod:  http.MethodGet,
+		ExpAuthMethod:  "ApiKey", // TODO perhaps use const or enum
+		ExpBody:        "",
+		ExpContentType: "", // TODO Are these required
+	}
 	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
-	expBody := ""
-	expContentType := "" // TODO Are these required
-	testCases := []struct {
-		testName string
-		expError error
-		expOut   string
-		resp     struct {
-			Status int
-			Body   string
-		}
-	}{
-		{testName: "Get all followed feeds: valid",
+	testCases := []TestCase{
+		{
+			name:     "Get all followed feeds: valid",
 			expError: nil,
 			expOut: `[
 	{
@@ -305,40 +247,9 @@ func TestGetAllFollowedFeeds(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
-		t.Run(tc.testName, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			// validate request well formed
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != expURLPath {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			var out bytes.Buffer
 			err := getAllFollowedFeedsAction(&out, url, ApiKey)
@@ -353,28 +264,21 @@ func TestGetAllFollowedFeeds(t *testing.T) {
 			if tc.expOut != out.String() {
 				t.Errorf("Expected: %q, \n\tgot: %q\n", tc.expOut, out.String())
 			}
-
 		})
 	}
 }
 func TestFollowFeed(t *testing.T) {
-	expURLPath := "/feed_follows"
-	expHTTPMethod := http.MethodPost
-	expAuthMethod := "ApiKey"     // TODO perhaps use const or enum
+	e := ExpReq{
+		ExpUrlPath:     "/feed_follows",
+		ExpHTTPMethod:  http.MethodPost,
+		ExpAuthMethod:  "ApiKey", // TODO perhaps use const or enum,
+		ExpContentType: "application/json",
+		ExpBody:        `{"feed_id":"c5c9212c-57a3-4d68-b42e-addd951502c0"}` + string('\n'), //Encoder adds a newline char
+	}
 	ApiKey := "someFancy4|>11<3j" //TODO decide whether to test this and HOW
-	expContentType := "application/json"
-	expBody := `{"feed_id":"c5c9212c-57a3-4d68-b42e-addd951502c0"}` + string('\n') //Encoder adds a newline char
-	testCases := []struct {
-		testName string
-		expError error
-		expOut   string
-		feedID   string
-		resp     struct {
-			Status int
-			Body   string
-		}
-	}{
-		{testName: "Follow existing feed",
+	testCases := []TestCase{
+		{
+			name:     "Follow existing feed",
 			expError: nil,
 			expOut:   `{"ID":"c52d3a13-2245-4991-8012-8856417b706f","CreatedAt":"2024-02-26T17:47:09.099267Z","UpdatedAt":"2024-02-26T17:47:09.099268Z","UserID":"8f588151-5489-4668-bfff-8c50021c1160","FeedID":"c5c9212c-57a3-4d68-b42e-addd951502c0"}` + string('\n'),
 			feedID:   "c5c9212c-57a3-4d68-b42e-addd951502c0",
@@ -382,40 +286,9 @@ func TestFollowFeed(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.testName, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			// validate request well formed
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != expURLPath {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			var out bytes.Buffer
 			err := followFeedAction(&out, tc.feedID, url, ApiKey)
@@ -430,27 +303,20 @@ func TestFollowFeed(t *testing.T) {
 			if tc.expOut != out.String() {
 				t.Errorf("Expected: %q, \n\tgot: %q\n", tc.expOut, out.String())
 			}
-
 		})
 	}
 }
 
 func TestCreateUser(t *testing.T) {
-	expURLPath := "/users"
-	expHTTPMethod := http.MethodPost
-	expContentType := "application/json"
-	expBody := `{"name":"testUsername"}` + string('\n') //Encoder adds a newline char
-	testCases := []struct {
-		testName string
-		expError error
-		expOut   string
-		username string
-		resp     struct {
-			Status int
-			Body   string
-		}
-	}{
-		{testName: "Valid request",
+	e := ExpReq{
+		ExpUrlPath:     "/users",
+		ExpHTTPMethod:  http.MethodPost,
+		ExpContentType: "application/json",
+		ExpBody:        `{"name":"testUsername"}` + string('\n'), //Encoder adds a newline char
+	}
+	testCases := []TestCase{
+		{
+			name:     "Valid request",
 			expError: nil,
 			expOut: `{"ID":"001","CreatedAt":"testTime","UpdatedAt":"2019-10-28T08:23:38.310097076-04:00","Name":"TestName","ApiKey":"414141414141"}
 `,
@@ -465,35 +331,13 @@ func TestCreateUser(t *testing.T) {
 		//	}	,
 	}
 	for _, tc := range testCases {
-		t.Run(tc.testName, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			// validate request well formed
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != expURLPath {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			// tests
 			var out bytes.Buffer
-			err := createUserAction(&out, url, tc.username)
+			err := createUserAction(&out, url, tc.username, true)
 			if err != nil {
 				if tc.expError == nil {
 					t.Fatalf("Expected no error, got: %q.\n", err)
@@ -510,25 +354,17 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestAddFeed(t *testing.T) {
-	expURLPath := "/feeds"
-	expHTTPMethod := http.MethodPost
-	expContentType := "application/json"
-	expAuthMethod := "ApiKey"                                          // TODO perhaps use const or enum
-	expBody := `{"name":"testName","url":"testingURL"}` + string('\n') //Encoder adds a newline char
+	e := ExpReq{
+		ExpUrlPath:     "/feeds",
+		ExpHTTPMethod:  http.MethodPost,
+		ExpContentType: "application/json",
+		ExpAuthMethod:  "ApiKey",
+		ExpBody:        `{"name":"testName","url":"testingURL"}` + string('\n'), //Encoder adds a newline char
+	}
 	// TODO check header is present?AUTH...
-	testCases := []struct {
-		name     string
-		expError error
-		expOut   string
-		feedName string
-		feedURL  string
-		resp     struct {
-			Status int
-			Body   string
-		}
-		closeServer bool
-	}{
-		{name: "add valid feed",
+	testCases := []TestCase{
+		{
+			name:     "add valid feed",
 			expError: nil,
 			expOut: `{
 			"feed": {"id":"1", "CreatedAt":"someTime",
@@ -551,38 +387,7 @@ func TestAddFeed(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			// validate request
-			url, cleanup := mockServer(
-				func(w http.ResponseWriter, r *http.Request) {
-					if r.URL.Path != expURLPath {
-						t.Errorf("Poor request! Expected path: %q, got: %q", expURLPath, r.URL.Path)
-					}
-					if r.Method != expHTTPMethod {
-						t.Errorf("Poor request! Expected method: %q, got: %q", expHTTPMethod, r.Method)
-					}
-					authValue := r.Header.Get("Authorization")
-					if authValue == "" {
-						t.Fatal("No header provided")
-					}
-					authMethod := strings.Split(authValue, " ")[0]
-					if authMethod != expAuthMethod {
-						t.Fatalf("Incorrect authorization method, expected: %v, got: %v\n", expAuthMethod, authMethod)
-					}
-					body, err := io.ReadAll(r.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-					r.Body.Close()
-					if string(body) != expBody {
-						t.Errorf("Poor request! Expected body: %q, got: %q", expBody, string(body))
-					}
-					contentType := r.Header.Get("Content-Type")
-					if contentType != expContentType {
-						t.Errorf("Poor request! Expected Content-Type: %q, got: %q", expContentType, contentType)
-					}
-
-					w.WriteHeader(tc.resp.Status)
-					fmt.Fprintln(w, tc.resp.Body)
-				})
+			url, cleanup := mockServer(checkReq(t, e, tc))
 			defer cleanup()
 			// test function
 			var out bytes.Buffer
