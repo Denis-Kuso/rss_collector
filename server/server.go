0abcd7e5ae7eada51476b520cb57e4f27d70d709
Denis Kuso <sourcework.online@keemail.me>
2025-01-15 20:54:52 +0000

    add graceful shutdown to server

--- server/server.go
+++ server/server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -15,6 +20,26 @@ func (s *StateConfig) serve() error {
 		Handler:           s.setupRoutes(),
 	}
 	go worker(s.DB, s.WorkOpts.WorkersBreak*time.Second, int(s.WorkOpts.NumWorkers))
+	shutdownErr := make(chan error)
+	go func() {
+		quit := make(chan os.Signal, 1) // unbuffered chanel might not receive
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		log.Printf("Shutting down server: %s", s.String())
+		const gracePeriod time.Duration = 10 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), gracePeriod)
+		defer cancel()
+		shutdownErr <- server.Shutdown(ctx)
+	}()
 	log.Printf("Serving on port: %s\n", s.PortNum)
-	return server.ListenAndServe()
+	err := server.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	err = <-shutdownErr
+	if err != nil { // if shutdown fails
+		return err
+	}
+	log.Printf("server stopped")
+	return nil
 }
