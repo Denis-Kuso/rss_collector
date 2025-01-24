1620fc9ef6499e07b40d31d72fddefcea88d0540
Denis Kuso <sourcework.online@keemail.me>
2025-01-24 11:12:58 +0000

    add stack trace to logs on serverError

--- server/helpers.go
+++ server/helpers.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"runtime/debug"
 )
 
 func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
@@ -33,6 +35,7 @@ func (a *app) logError(r *http.Request, err error) {
 }
 
 func (app *app) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	err = fmt.Errorf("%w: %s", err, string(debug.Stack()))
 	app.logError(r, err)
 
 	msg := "OOPS! Ran into a problem and could not process your request"
