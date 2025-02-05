2334be81e028bf997eb1a4c7456c8da6f3c505f4
Denis Kuso <sourcework.online@keemail.me>
2025-02-05 12:58:00 +0000

    copy missing queries file

---
+++ internal/database/users.sql
@@ -0,0 +1,16 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, api_key)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    encode(sha256(random()::text::bytea), 'hex'))
+RETURNING *;
+
+-- name: GetUserByAPIKey :one
+SELECT * FROM users WHERE api_key = $1;
+
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = $1;
+
